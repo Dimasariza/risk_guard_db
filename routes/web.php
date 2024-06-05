da2b4cda96e419b65443b7ba17e0eda33470b95d
Dimasariza <roganda.sihombing11@gmail.com>
2024-06-05 21:16:48 +0700

    add api routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('/test', function () {
+        return response()->json([
+            "message" => "test success",
+            "data" => "Ready to toest"
+        ]);
+    });
+});
